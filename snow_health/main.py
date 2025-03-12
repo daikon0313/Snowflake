a88016abd203b695a4850bc0eb0c540669bcc11e
daisuke harato <d.harato@datumstudio.jp>
2025-03-12 13:46:34 +0900

    add

--- snow_health/main.py
+++ snow_health/main.py
@@ -19,6 +19,29 @@ with open(private_key_path, "rb") as key_file:
 snowflake_connection_cfg["private_key"] = private_key
 session = Session.builder.configs(snowflake_connection_cfg).create()
 
-# テストクエリ実行
-df = session.sql("SELECT 1 AS id").collect()
-st.write(df)
+# SQLファイルを読み込む関数
+def load_sql(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
+
+# クエリ読み込み
+unused_tables_query = load_sql("sql/unused_tables.sql")
+unused_views_query = load_sql("sql/unused_views.sql")
+
+st.title("未使用テーブル・ビューの特定")
+
+# 未使用テーブル
+st.subheader("使用されていないテーブル")
+st.code(unused_tables_query, language="sql")
+
+st.write("クエリを実行中...")
+unused_tables_df = session.sql(unused_tables_query).collect()
+st.dataframe(unused_tables_df)
+
+# 未使用ビュー
+st.subheader("使用されていないビュー")
+st.code(unused_views_query, language="sql")
+
+st.write("クエリを実行中...")
+unused_views_df = session.sql(unused_views_query).collect()
+st.dataframe(unused_views_df)
---
+++ snow_health/sql/unused_views.sql
@@ -0,0 +1,46 @@
+WITH access_history_flattened AS (
+    SELECT
+        access_history.query_id,
+        access_history.query_start_time,
+        access_history.user_name,
+        objects_accessed.value:objectId::integer AS table_id,
+        objects_accessed.value:objectName::text AS object_name,
+        objects_accessed.value:objectDomain::text AS object_domain,
+        objects_accessed.value:columns AS columns_array
+    FROM snowflake.account_usage.access_history, LATERAL FLATTEN(access_history.base_objects_accessed) AS objects_accessed
+),
+table_access_history AS (
+    SELECT
+        query_id,
+        query_start_time,
+        user_name,
+        object_name AS fully_qualified_table_name,
+        table_id
+    FROM access_history_flattened
+    WHERE object_domain = 'View'
+),
+table_access_summary AS (
+    SELECT
+        table_id,
+        MAX(query_start_time) AS last_accessed_at,
+        MAX_BY(user_name, query_start_time) AS last_accessed_by,
+        MAX_BY(query_id, query_start_time) AS last_query_id
+    FROM table_access_history
+    GROUP BY 1
+),
+table_storage_metrics AS (
+    SELECT
+        id AS table_id,
+        table_catalog || '.' || table_schema || '.' || table_name AS fully_qualified_table_name,
+        (active_bytes + time_travel_bytes + failsafe_bytes + retained_for_clone_bytes)/POWER(1024,4) AS total_storage_tb
+    FROM snowflake.account_usage.table_storage_metrics
+    WHERE NOT deleted
+)
+SELECT
+    table_storage_metrics.*,
+    table_access_summary.* EXCLUDE (table_id)
+FROM table_storage_metrics
+LEFT JOIN table_access_summary
+    ON table_storage_metrics.table_id = table_access_summary.table_id
+WHERE COALESCE(last_accessed_at, DATE'1900-01-01') < (CURRENT_DATE - 30)
+ORDER BY table_storage_metrics.total_storage_tb DESC;
